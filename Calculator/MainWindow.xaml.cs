beadb9561ffab136193ec75a18bde99eb904ef9b
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2025-03-07 21:59:22 +0200

    I have added the possibility to input calculations from the keyboard.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -23,6 +23,46 @@ namespace Calculator
             InitializeComponent();
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                Display.Text = Display.Text == "0" ? ((int)e.Key - (int)Key.D0).ToString() : Display.Text + ((int)e.Key - (int)Key.D0).ToString();
+            }
+            else if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                Display.Text = Display.Text == "0" ? (e.Key.ToString().Substring(e.Key.ToString().Length - 1)) : Display.Text + (e.Key.ToString().Substring(e.Key.ToString().Length - 1));
+            }
+            else if (e.Key == Key.Add || e.Key == Key.Subtract || e.Key == Key.Multiply || e.Key == Key.Divide || e.Key == Key.OemPlus || e.Key == Key.OemMinus || e.Key == Key.OemQuestion)
+            {
+                string operatorSymbol = string.Empty;
+
+                if (e.Key == Key.Add || e.Key == Key.OemPlus) operatorSymbol = "+";
+                if (e.Key == Key.Subtract || e.Key == Key.OemMinus) operatorSymbol = "-";
+                if (e.Key == Key.Multiply) operatorSymbol = "×";
+                if (e.Key == Key.Divide) operatorSymbol = "÷";  
+                if (e.Key == Key.OemQuestion) operatorSymbol = "%"; 
+
+                _currentValue = double.Parse(Display.Text);
+                _currentOperation = operatorSymbol;
+                Display.Clear();
+            }
+            else if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                Button_Equals_Click(this.FindName("Button_Equals") as Button, null);
+            }
+            else if (e.Key == Key.Back)
+            {
+                if (Display.Text.Length > 1)
+                    Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+                else
+                    Display.Text = "0";
+            }
+            else if (e.Key == Key.Escape)
+            {
+                Button_Clear_Click(this.FindName("Button_Clear") as Button, null);
+            }
+        }
 
         private void Button_MC_Click(object sender, RoutedEventArgs e) { _memory = 0; }
         private void Button_MR_Click(object sender, RoutedEventArgs e) { Display.Text = _memory.ToString(); }
